244dfae45542b363691effacfadc4773e8590c74
Antonio <tzamora@gmail.com>
2014-08-25 00:23:37 -0600

    Ahora si descubrimos porque es que había tantos problemas con el código que tenia hecho. Ahora corregimos la manera en la que funcionaban las llamadas a las rutinas y probamos mejor como mover los items en la función de spin

---
+++ Assets/src/MachinePanelController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MachinePanelController : MonoBehaviour {
+
+	public List<SlotPanelController> Slots;
+
+	private bool playingGame = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void playGameButtonHandler(){
+
+		StartCoroutine (playSlotsRoutine ());
+	}
+
+	public IEnumerator playSlotsRoutine (){
+
+		// first play the first slot
+
+		if (!playingGame) 
+		{
+			playingGame = true;
+
+			Debug.Log("PRENDIENDO!!@@");
+
+			yield return StartCoroutine (playSlot(Slots[0]));
+
+	//		foreach (SlotPanelController slot in Slots) {
+	//		
+	//			yield return StartCoroutine (playSlot(slot));
+	//
+	//		}
+
+			playingGame = false;
+
+			Debug.Log("$$%%APAGANDO");
+
+		}
+	}
+
+	public IEnumerator playSlot(SlotPanelController slot){
+
+		yield return StartCoroutine(slot.spinRoutine (13));
+
+		yield return new WaitForSeconds (2);
+
+		Debug.Log ("detener el slot");
+
+		yield return StartCoroutine(slot.spinRoutine (0));
+	}
+}
--- Assets/src/SlotPanelController.cs
+++ Assets/src/SlotPanelController.cs
@@ -18,13 +18,15 @@ public class SlotPanelController : MonoBehaviour {
 
 	public int side = -1;
 
+	private float currentSpeed = 0;
+
 	void Start() 
 	{
 		// first get some slot items
 
 		renderSlotItems ();
 
-		Spin ();
+		//Spin ();
 	}
 
 	public void renderSlotItems()
@@ -32,26 +34,32 @@ public class SlotPanelController : MonoBehaviour {
 		// first get the set of items
 		slotItemsPool = GameContext.Get.SlotItemsFactory.buildSlotItems();
 
+		currentSlotItems = new List<SlotItemController>();
+
 		// begin one item up of the first visible item thats why i = -1
 		// and add one more at the end thats why itemsPerSlot + 1
 
 		for (int i = 0; i < itemsPerSlot + 2; i++)
 		{
-			RectTransform rect = slotItemsPool[i].GetComponent<RectTransform>();
+			// get the item to add into the slot
 
-			slotItemsPool[i].transform.parent = slotPanelPivot;
+			SlotItemController slotItem = slotItemsPool[slotItemsPool.Count-1];
+
+			Debug.Log("el item: " + slotItem.name + " va a ir de posicion: " + i);
+
+			slotItemsPool.Remove(slotItem);
+
+			RectTransform rect = slotItem.GetComponent<RectTransform>();
+
+			slotItem.transform.parent = slotPanelPivot;
 
 			float pos = (side * verticalGap * (i-1));
 
-			Debug.Log("viene la posicion " + pos);
-			
 			rect.localPosition = new Vector3(0f, pos, 0f);
 
-			currentSlotItems.Add(slotItemsPool[i]);
+			currentSlotItems.Insert(0,slotItem);
 
-			slotItemsPool[i].gameObject.SetActive(true);
-
-			slotItemsPool.Remove(slotItemsPool[i]);
+			slotItem.gameObject.SetActive(true);
 		}
 	}
 
@@ -60,35 +68,130 @@ public class SlotPanelController : MonoBehaviour {
 
 	}
 
-	public void Spin()
+//	public void Spin(float speed)
+//	{
+//		StartCoroutine(spinRoutine)
+//	}
+
+	public IEnumerator spinRoutine(float speed)
 	{
 		// make each one of the items to spin
+		
+		StartCoroutine(UnityUtils.LerpAction(2, delegate(float t) {
+			
+			currentSpeed = Mathf.Lerp(currentSpeed, speed, t);
+			
+			//Debug.Log ("current: " + currentSpeed + " :: speed : " + speed + " :: t: " + t);
+			
+		}));
 
-		StartCoroutine (SpinSlotItem ());
+		yield return StartCoroutine (SpinSlotItem (speed));
 	}
 
-	private IEnumerator SpinSlotItem()
+	private IEnumerator SpinSlotItem(float speed)
 	{
-		while (true) // per frame
+		bool spinSlot = true;
+
+		while (spinSlot) // per frame
 		{
+//			while(currentSlotItems.Count > 0)
+//			{
+//				if(currentSpeed == speed)
+//				{
+//					spinSlot = false;
+//					
+//					// [TODO] revisar que el slot machine quede en un estado en el que se pueda 
+//					// apreciar bien el item ganado
+//					
+//					break;
+//				}
+//
+//				SlotItemController slotItem = currentSlotItems.Peek();
+//
+//				slotItem.transform.position -= new Vector3(0f,currentSpeed,0f);
+//
+//				// if it surpases theslotItemsPool[i].gameObject.SetActive(true); size of the slot view then return it back again to the pool
+//
+//				if(slotItem.transform.localPosition.y < (transform.localPosition.y - (itemsPerSlot * verticalGap)) -100)
+//				{
+//					//Debug.Break();
+//
+//					// add the last item back to the pool
+//					//slotItemsPool.Insert(0,slotItem);
+//
+//					slotItem = currentSlotItems.Dequeue();
+//
+//					slotItemsPool.Enqueue(slotItem);
+//
+//					slotItem.gameObject.SetActive(false);
+//
+//					slotItem.transform.parent = null;
+//
+//					// insert the new slot item at the end
+//					//currentSlotItems.Insert(0,slotItemsPool[currentSlotItems.Count - 1]);
+//
+//
+//					slotItem = slotItemsPool.Dequeue();
+//
+//					currentSlotItems.Enqueue(slotItem);
+//
+//					slotItem.gameObject.SetActive(true);
+//
+//					slotItem.transform.parent = slotPanelPivot;
+//
+//					slotItem.transform.localPosition = 
+//						new Vector3(0f,(side * verticalGap * (-1)),0f);
+//
+//
+//				}
+//
+//
+//				yield return null;
+//			}
+
 			for(int i = 0; i < currentSlotItems.Count; i++)
 			{
 				SlotItemController slotItem = currentSlotItems[i];
 
-				slotItem.transform.position -= new Vector3(0f,SlotSpeed,0f);
+				//Debug.Log("vamos a ver que item viene: " + currentSlotItems.Count);
+
+				if(currentSpeed == speed)
+				{
+					spinSlot = false;
+
+					// [TODO] revisar que el slot machine quede en un estado en el que se pueda 
+					// apreciar bien el item ganado
+
+					break;
+				}
+
+				slotItem.transform.position -= new Vector3(0f,currentSpeed,0f);
+
+				// movimiento
+				/*slotItem.transform.position = 
+					Vector3.Lerp(
+						slotItem.transform.position,
+						slotItem.transform.position-new Vector3(0f, currentSpeed*5, 0f),
+						Time.deltaTime
+					);*/
+
+				//slotItem.gameObject.transform.Translate(new Vector3(0f,-currentSpeed,0f));
+
+				//Debug.Log(currentSpeed);
 
 				// if it surpases theslotItemsPool[i].gameObject.SetActive(true); size of the slot view then return it back again to the pool
 
-				Debug.Log("[ " + slotItem.transform.localPosition + " ]");
-				Debug.Log("[ " + (transform.localPosition.y - (itemsPerSlot * verticalGap)) + " ]");
-
 				if(slotItem.transform.localPosition.y < (transform.localPosition.y - (itemsPerSlot * verticalGap)) -100)
 				{
+					//Debug.Break();
+
 					// add the last item back to the pool
 					slotItemsPool.Insert(0,slotItem);
 
 					slotItem.gameObject.SetActive(false);
-					
+
+					slotItem.transform.parent = null;
+
 					currentSlotItems.Remove(slotItem);
 
 					// insert the new slot item at the end
@@ -98,18 +201,20 @@ public class SlotPanelController : MonoBehaviour {
 
 					slotItemsPool.RemoveAt(slotItemsPool.Count - 1);
 
-					// place the 
-
 					currentSlotItems[0].transform.parent = slotPanelPivot;
 
 					currentSlotItems[0].transform.localPosition = 
 						new Vector3(0f,(side * verticalGap * (-1)),0f);
+
+					//Debug.Break();
 				}
 
 			}
 
 			yield return null;		
 		}
+
+		Debug.Log("salimos del infierno de hilo");
 	}
 
 	public SlotItemController GetSlotItem(SlotItemPositionEnum itemPos)
